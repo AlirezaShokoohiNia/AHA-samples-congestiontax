458782354f397d32a731099d600f810f386085aa
AlirezaShokoohiNia <arshn@yahoo.com>
2025-11-22 23:13:40 +0330

    feat(rules): implement RedisRuleSetReader

--- src/AHA.CongestionTax.Application/Queries/Readers/RedisRuleSetReader.cs
+++ src/AHA.CongestionTax.Application/Queries/Readers/RedisRuleSetReader.cs
@@ -1,15 +1,24 @@
 namespace AHA.CongestionTax.Application.Queries.Readers
 {
+    using System.Text.Json;
+    using System.Threading.Tasks;
     using AHA.CongestionTax.Application.Queries.RuleSets;
     using StackExchange.Redis;
-    using System.Threading.Tasks;
 
     public class RedisRuleSetReader(IConnectionMultiplexer redis)
         : IRuleSetReader
     {
-        public Task<RuleSetQueryModel?> GetRulesForCityAsync(string city)
+        public async Task<RuleSetQueryModel?> GetRulesForCityAsync(string city)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(city))
+                throw new ArgumentException("City must be provided", nameof(city));
+
+            var db = redis.GetDatabase();
+            string key = $"toll:rules:city:{city}";
+
+            var json = await db.StringGetAsync(key);
+
+            return json.IsNullOrEmpty ? null : JsonSerializer.Deserialize<RuleSetQueryModel>(json!);
         }
     }
 }
