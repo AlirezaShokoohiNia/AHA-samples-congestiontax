425db14b4584bd1c48f037b6c6580a41310ba8c8
AlirezaShokoohiNia <arshn@yahoo.com>
2025-12-02 11:42:41 +0330

    docs(cqrs-read): add xml doc to IQuery

--- src/Application/Abstractions/Queries/IQuery.cs
+++ src/Application/Abstractions/Queries/IQuery.cs
@@ -1,4 +1,9 @@
 namespace AHA.CongestionTax.Application.Abstractions.Queries
 {
+    /// <summary>
+    /// Represents a marker interface for application queries
+    /// in the CQRS pattern. Queries encapsulate requests for
+    /// data retrieval and must not alter system state.
+    /// </summary>
     public interface IQuery { }
 }
