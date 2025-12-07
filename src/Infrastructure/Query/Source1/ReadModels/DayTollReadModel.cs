c6f77e65466fc29d9f4ca9c8e4d28de5f8796074
AlirezaShokoohiNia <arshn@yahoo.com>
2025-12-07 11:53:22 +0330

    feat(infra): add initializer to properties of  Source1.DayTollReadModel

--- src/Infrastructure/Query/Source1/ReadModels/DayTollReadModel.cs
+++ src/Infrastructure/Query/Source1/ReadModels/DayTollReadModel.cs
@@ -8,27 +8,27 @@ namespace AHA.CongestionTax.Infrastructure.Query.Source1.ReadModels
         /// <summary>
         /// Unique persistence identifier of the vehicle in the database.
         /// </summary>
-        public int VehicleId { get; set; }
+        public int VehicleId { get; init; }
 
         /// <summary>
         /// Unique license plate identifier for the vehicle.
         /// </summary>
-        public string LicensePlate { get; private set; } = default!;
+        public string LicensePlate { get; init; } = default!;
 
         /// <summary>
         /// The calendar date for which tolls are being aggregated.
         /// </summary>
-        public DateOnly Date { get; private set; }
+        public DateOnly Date { get; init; }
 
         /// <summary>
         /// The city in which the passes occurred.
         /// </summary>
-        public string City { get; private set; } = default!;
+        public string City { get; init; } = default!;
 
         /// <summary>
         /// The total congestion tax fee calculated for the day.
         /// </summary>
-        public int TotalFee { get; private set; }
+        public int TotalFee { get; init; }
 
     }
 }
