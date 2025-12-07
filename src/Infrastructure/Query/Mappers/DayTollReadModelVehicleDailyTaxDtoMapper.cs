d6835bc8362803beead3678075e34ddb693042a2
AlirezaShokoohiNia <arshn@yahoo.com>
2025-12-07 11:55:17 +0330

    feat(cqrs-read): add DayTollReadModelVehicleDailyTaxDtoMapper

---
+++ src/Infrastructure/Query/Mappers/DayTollReadModelVehicleDailyTaxDtoMapper.cs
@@ -0,0 +1,33 @@
+namespace AHA.CongestionTax.Infrastructure.Query.Mappers
+{
+    using AHA.CongestionTax.Application.DTOs;
+    using AHA.CongestionTax.Infrastructure.Query.Source1.ReadModels;
+
+    /// <summary>
+    /// Maps a DayToll read model into a VehicleDailyTax dto.
+    /// </summary>
+    public class DayTollReadModelVehicleDailyTaxDtoMapper
+        : IReadModelToDtoMapper<DayTollReadModel, VehicleDailyTaxDto>
+    {
+        /// <summary>
+        /// Converts a single daytoll read model into a VehicleDailyTax DTO.
+        /// </summary>
+        public static VehicleDailyTaxDto Map(DayTollReadModel readModel)
+        {
+
+            return new VehicleDailyTaxDto
+            {
+                LicensePlate = readModel.LicensePlate,
+                Date = readModel.Date,
+                City = readModel.City,
+                Tax = readModel.TotalFee
+            };
+        }
+
+        /// <summary>
+        /// Converts a collection of daytoll read models into VehicleDailyTax DTOs.
+        /// </summary>
+        public static IReadOnlyCollection<VehicleDailyTaxDto> MapMany(IEnumerable<DayTollReadModel> readModels)
+            => [.. readModels.Select(Map)];
+    }
+}
