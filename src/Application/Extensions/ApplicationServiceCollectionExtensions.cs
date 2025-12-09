71bb2600ede3a8ed9fd296a530afbda98d12936b
AlirezaShokoohiNia <arshn@yahoo.com>
2025-12-10 02:57:02 +0330

    feat(application): expose DI extention.
    - Added needed package
    - Added ApplicationServiceCollectionExtensions extension

---
+++ src/Application/Extensions/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+namespace AHA.CongestionTax.Application.Extensions
+{
+    using AHA.CongestionTax.Application.Abstractions.Command;
+    using AHA.CongestionTax.Application.Abstractions.Query;
+    using AHA.CongestionTax.Application.Commands;
+    using AHA.CongestionTax.Application.DTOs;
+    using AHA.CongestionTax.Application.Queries;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ApplicationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            _ = services.AddScoped<ICommandHandler<RegisterPassCommand, int>, RegisterPassCommandHandler>()
+                        .AddScoped<ICommandHandler<RegisterVehicleCommand, int>, RegisterVehicleCommandHandler>()
+                        .AddScoped<IQueryHandler<GetRuleSetQuery, RuleSetDto>, GetRuleSetQueryHandler>()
+                        .AddScoped<IQueryHandler<GetVehicleQuery, VehicleDto>, GetVehicleQueryHandler>()
+                        .AddScoped<IQueryHandler<GetVehicleDailyTaxRecordsQuery, IReadOnlyCollection<VehicleDailyTaxDto>>, GetVehicleDailyTaxRecordsQueryHandler>()
+                        .AddScoped<IQueryHandler<GetVehicleWeeklyTotalTaxQuery, VehicleTotalTaxDto>, GetVehicleWeeklyTotalTaxQueryHandler>();
+
+            return services;
+        }
+    }
+
+}
