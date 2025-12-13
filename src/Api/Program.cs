de2a4650b0c8ada23659d4c87b3b5497cb9956aa
AlirezaShokoohiNia <arshn@yahoo.com>
2025-12-13 05:54:50 +0330

    fix(api): refactor infrastructure registeration using new extensions

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,13 +1,29 @@
 using AHA.CongestionTax.Api;
 using AHA.CongestionTax.Application.Extensions;
 using AHA.CongestionTax.Infrastructure.Extensions;
+using AHA.CongestionTax.Infrastructure.Query.Source2.Options;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Register Application + Infrastructure
+// Application services
 builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
+
+// Infrastructure: write side (AppDbContext)
+builder.Services.AddInfrastructureData(opts =>
+    opts.UseSqlite(builder.Configuration.GetConnectionString("WriteDbConnection")));
+
+// Infrastructure: read side (QueryDbContext)
+builder.Services.AddInfrastructureSource1(opts =>
+    opts.UseSqlite(builder.Configuration.GetConnectionString("ReadDbConnection")));
+
+// Infrastructure: JSON/file sources
+builder.Services.AddInfrastructureSource2();
+
+//  Infrastructure: Options
+builder.Services.Configure<RuleSetOptions>(
+    builder.Configuration.GetSection("RuleSet"));
 
 // Swagger
 builder.Services.AddEndpointsApiExplorer();
