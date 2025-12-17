8e130a29a5379d047a3c36c6b7fc5924bb45c9f7
AlirezaShokoohiNia <arshn@yahoo.com>
2025-12-17 11:16:32 +0330

    feat(migrations): add InitialCreate migrations using dotnet ef migrations tool

---
+++ src/Infrastructure.Migrations/Sqlite/20251217073236_InitialCreate.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations.SQLite
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LicensePlate = table.Column<string>(type: "TEXT", nullable: false),
+                    VehicleType = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DayTolls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    VehicleId = table.Column<int>(type: "INTEGER", nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    TotalFee = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DayTolls", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DayTolls_Vehicles_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "Vehicles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PassRecord",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Time = table.Column<TimeOnly>(type: "TEXT", nullable: false),
+                    DayTollId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PassRecord", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PassRecord_DayTolls_DayTollId",
+                        column: x => x.DayTollId,
+                        principalTable: "DayTolls",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DayTolls_VehicleId",
+                table: "DayTolls",
+                column: "VehicleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PassRecord_DayTollId",
+                table: "PassRecord",
+                column: "DayTollId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PassRecord");
+
+            migrationBuilder.DropTable(
+                name: "DayTolls");
+
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
