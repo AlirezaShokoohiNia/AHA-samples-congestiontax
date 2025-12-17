55fdd16233a8cf57fde2c488b76074df7add6d1b
AlirezaShokoohiNia <arshn@yahoo.com>
2025-12-17 10:23:21 +0330

    test(migrations): add fixture to share the Sqlite database fixture across multiple test classes.

---
+++ src/Infrastructure.Migrations.Tests/Sqlite/Fixtures/DatabaseCollectionFixture.cs
@@ -0,0 +1,11 @@
+
+namespace AHA.CongestionTax.Infrastructure.Migrations.Sqlite.Tests.Fixtures
+{
+    /// <summary>
+    /// Collection fixture for Sqlite database tests.  
+    /// Helps to share the DatabaseFixture across multiple test classes.
+    /// </summary>
+    [CollectionDefinition("Database collection")]
+    public class DatabaseCollectionFixture : ICollectionFixture<DatabaseFixture> { }
+
+}
