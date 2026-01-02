8cabecb26c5e67ca53fa9492a1a7a28ace0319a6
AlirezaShokoohiNia <arshn@yahoo.com>
2026-01-02 07:03:04 +0330

    test(adapter): add failing test for VehicleFreeRuleDtoToVehicleTypeAdapter  (#13)
    - Defined expected validation behavior for null/whitespace VehicleFreeRuleDto input

--- src/Application.Tests/Adapters/VehicleFreeRuleDtoToVehicleTypeAdapterTests.cs
+++ src/Application.Tests/Adapters/VehicleFreeRuleDtoToVehicleTypeAdapterTests.cs
@@ -23,6 +23,18 @@ namespace AHA.CongestionTax.Application.Adapters.Tests
             Assert.Equal(expected, result);
         }
 
+        [Fact]
+        public void Adapt_Should_Return_Unknown_For_Null_Or_Whitespace()
+        {
+            var dtoNull = new VehicleFreeRuleDto { VehicleType = null! };
+            var dtoWhitespace = new VehicleFreeRuleDto { VehicleType = "   " };
+
+            var resultNull = VehicleFreeRuleDtoToVehicleTypeAdapter.Adapt(dtoNull);
+            var resultWhitespace = VehicleFreeRuleDtoToVehicleTypeAdapter.Adapt(dtoWhitespace);
+
+            Assert.Equal(VehicleType.Unknown, resultNull);
+            Assert.Equal(VehicleType.Unknown, resultWhitespace);
+        }
     }
 
 }
